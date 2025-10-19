bcd43c4a77e601b18c8ed66433117c95962263d5
k.kosterin <kosterin-kirill@mail.ru>
2025-10-19 22:33:51 +0300

    Mark RMEntity class with @Entity annotation, configure @PrimaryKey for id field

--- app/src/main/java/dev/kiryao/rickandmorty/core/data/local/RMEntity.kt
+++ app/src/main/java/dev/kiryao/rickandmorty/core/data/local/RMEntity.kt
@@ -1,7 +1,11 @@
 package dev.kiryao.rickandmorty.core.data.local
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class RMEntity(
-    val id: Int,
+    @PrimaryKey val id: Int,
     val name: String,
     val status: String,
     val species: String,
